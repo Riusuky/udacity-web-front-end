76a3462ccf9dc4dcd178d62d548bdcab3b51983d
Riusuky <Riusuky>
2017-06-05 08:22:45 -0300

    Added initial project 7 files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,187 +1,221 @@
+'use strict';
+
+var ngrok = require('ngrok');
+
 module.exports = function(grunt) {
 
-  portfolioPath = "4.Build_a_Portfolio_Site/";
+    var portfolioPath = "4.Build_a_Portfolio_Site/";
 
-  grunt.initConfig({
-    responsive_images: {
-      portfolio: {
-        options: {
-          engine: "im",
-          newFilesOnly: true,
-          sizes: [
-            {
-              name: "small",
-              width: 550,
-              quality: 70
+    grunt.initConfig({
+        responsive_images: {
+          portfolio: {
+            options: {
+              engine: "im",
+              newFilesOnly: true,
+              sizes: [
+                {
+                  name: "small",
+                  width: 550,
+                  quality: 70
+                },
+                {
+                  name: "medium",
+                  width: 750,
+                  quality: 70
+                },
+                {
+                  name: "large",
+                  width: 940,
+                  quality: 70
+                },
+                {
+                  name: "large",
+                  suffix: "-2x",
+                  width: 1880,
+                  quality: 70
+                }
+              ]
             },
-            {
-              name: "medium",
-              width: 750,
-              quality: 70
-            },
-            {
-              name: "large",
-              width: 940,
-              quality: 70
-            },
-            {
-              name: "large",
-              suffix: "-2x",
-              width: 1880,
-              quality: 70
-            }
-          ]
-        },
-        files: [{
-          expand: true,
-          src: ["*.{gif,jpg,png}"],
-          cwd: portfolioPath+"src/img/resizable/",
-          dest: portfolioPath+"img/"
-        }]
-      }
-    },
-
-    imagemin: {
-      options: {
-        optimizationLevel: 5
-      },
-      portfolio: {
-        files: [{
-          expand: true,
-          cwd: portfolioPath+"img/",
-          src: ["**/*.{png,jpg,gif,svg}"],
-          dest: portfolioPath+"img/"
-        }]
-      }
-    },
-
-    googlefonts: {
-      portfolio: {
-        options: {
-          fontPath: "fonts/",
-          httpPath: "../fonts/",
-          cssFile: portfolioPath+"css/fonts.css",
-          fonts: [
-            {
-              family: "Julius Sans One",
-              styles: [
-                400
-              ],
-              subsets: [
-                "latin"
-              ],
-            }
-          ]
-        }
-      }
-    },
-
-    regex_replacer: {
-      fixPortfolioUrls: {
-        src: portfolioPath+"css/fonts.css",
-        regex: /url\(\'[\s\w/.-]*\'\)/g,
-        replacement: function (match) {
-          return match.slice(0, 4)+match.slice(5, -2)+")";
-        }
-      },
-      fixPortfolioStrings: {
-        src: portfolioPath+"css/fonts.css",
-        regex: /\'[\s\w/.-]*\'/g,
-        replacement: function (match) {
-          return "\""+match.slice(1, -1)+"\"";
-        }
-      }
-    },
-
-    postcss: {
-      options: {
-          processors: [
-              require("autoprefixer")
-          ]
-      },
-      portfolio: {
-        expand: true,
-        cwd: portfolioPath+"src/css/",
-        src: ["*.css", "!normalize.css"],
-        dest: portfolioPath+"css/"
-      }
-    },
-
-    cssmin: {
-      portfolio: {
-        files: [{
-          expand: true,
-          cwd: portfolioPath+"css/",
-          src: ["*.css", "!*.min.css"],
-          dest: portfolioPath+"css/",
-          ext: ".min.css"
-        }]
-      }
-    },
-
-    htmlmin: {
-      portfolio: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
-        },
-        files: [
-          {
-            expand: true,
-            cwd: portfolioPath+"src/",
-            src: ["*.html"],
-            dest: portfolioPath
+            files: [{
+              expand: true,
+              src: ["*.{gif,jpg,png}"],
+              cwd: portfolioPath+"src/img/resizable/",
+              dest: portfolioPath+"img/"
+            }]
           }
-        ]
-      }
-    },
+        },
 
-    clean: {
-      portfolio: [portfolioPath+"css/", portfolioPath+"img/"],
-      portfolioFonts: [portfolioPath+"fonts/*"],
-      fonts: ["fonts/"]
-    },
+        imagemin: {
+          options: {
+            optimizationLevel: 5
+          },
+          portfolio: {
+            files: [{
+              expand: true,
+              cwd: portfolioPath+"img/",
+              src: ["**/*.{png,jpg,gif,svg}"],
+              dest: portfolioPath+"img/"
+            }]
+          }
+        },
 
-    copy: {
-      portfolioCSS: {
-        files: [
-          {
+        googlefonts: {
+          portfolio: {
+            options: {
+              fontPath: "fonts/",
+              httpPath: "../fonts/",
+              cssFile: portfolioPath+"css/fonts.css",
+              fonts: [
+                {
+                  family: "Julius Sans One",
+                  styles: [
+                    400
+                  ],
+                  subsets: [
+                    "latin"
+                  ],
+                }
+              ]
+            }
+          }
+        },
+
+        regex_replacer: {
+          fixPortfolioUrls: {
+            src: portfolioPath+"css/fonts.css",
+            regex: /url\(\'[\s\w/.-]*\'\)/g,
+            replacement: function (match) {
+              return match.slice(0, 4)+match.slice(5, -2)+")";
+            }
+          },
+          fixPortfolioStrings: {
+            src: portfolioPath+"css/fonts.css",
+            regex: /\'[\s\w/.-]*\'/g,
+            replacement: function (match) {
+              return "\""+match.slice(1, -1)+"\"";
+            }
+          }
+        },
+
+        postcss: {
+          options: {
+              processors: [
+                  require("autoprefixer")
+              ]
+          },
+          portfolio: {
             expand: true,
-            src: ["*/**", "normalize.css"],
             cwd: portfolioPath+"src/css/",
+            src: ["*.css", "!normalize.css"],
             dest: portfolioPath+"css/"
           }
-        ]
-      },
-      portfolioImages: {
-        files: [
-          {
-            expand: true,
-            src: ["*.{jpg,gif,png,svg}"],
-            cwd: portfolioPath+"src/img/",
-            dest: portfolioPath+"img/"
+        },
+
+        cssmin: {
+          portfolio: {
+            files: [{
+              expand: true,
+              cwd: portfolioPath+"css/",
+              src: ["*.css", "!*.min.css"],
+              dest: portfolioPath+"css/",
+              ext: ".min.css"
+            }]
           }
-        ]
-      },
-      portfolioFonts: {
-        files: [
-          {
-            expand: true,
-            src: ["fonts/**"],
-            dest: portfolioPath
+        },
+
+        htmlmin: {
+          portfolio: {
+            options: {
+              removeComments: true,
+              collapseWhitespace: true
+            },
+            files: [
+              {
+                expand: true,
+                cwd: portfolioPath+"src/",
+                src: ["*.html"],
+                dest: portfolioPath
+              }
+            ]
           }
-        ]
-      }
-    }
-  });
+        },
 
-  require("load-grunt-tasks")(grunt);
+        clean: {
+          portfolio: [portfolioPath+"css/", portfolioPath+"img/"],
+          portfolioFonts: [portfolioPath+"fonts/*"],
+          fonts: ["fonts/"]
+        },
 
-  grunt.registerTask("default", []);
+        copy: {
+          portfolioCSS: {
+            files: [
+              {
+                expand: true,
+                src: ["*/**", "normalize.css"],
+                cwd: portfolioPath+"src/css/",
+                dest: portfolioPath+"css/"
+              }
+            ]
+          },
+          portfolioImages: {
+            files: [
+              {
+                expand: true,
+                src: ["*.{jpg,gif,png,svg}"],
+                cwd: portfolioPath+"src/img/",
+                dest: portfolioPath+"img/"
+              }
+            ]
+          },
+          portfolioFonts: {
+            files: [
+              {
+                expand: true,
+                src: ["fonts/**"],
+                dest: portfolioPath
+              }
+            ]
+          }
+        },
 
-  grunt.registerTask("portfolio-fonts", ["clean:portfolioFonts", "googlefonts:portfolio", "regex_replacer:fixPortfolioUrls", "regex_replacer:fixPortfolioStrings", "copy:portfolioFonts", "clean:fonts"]);
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold: 40
+            },
+            local: {
+                options: { strategy: "desktop" }
+            },
+            mobile: {
+                options: { strategy: "mobile" }
+            }
+        }
+    });
 
-  grunt.registerTask("portfolio-css", ["copy:portfolioCSS", "postcss:portfolio", "cssmin:portfolio"]);
+    require("load-grunt-tasks")(grunt);
 
-  grunt.registerTask("portfolio", ["clean:portfolio", "portfolio-fonts", "htmlmin:portfolio", "portfolio-css", "copy:portfolioImages", "responsive_images:portfolio", "imagemin:portfolio"]);
+    grunt.registerTask("default", []);
+
+    grunt.registerTask("portfolio-fonts", ["clean:portfolioFonts", "googlefonts:portfolio", "regex_replacer:fixPortfolioUrls", "regex_replacer:fixPortfolioStrings", "copy:portfolioFonts", "clean:fonts"]);
+
+    grunt.registerTask("portfolio-css", ["copy:portfolioCSS", "postcss:portfolio", "cssmin:portfolio"]);
+
+    grunt.registerTask("portfolio", ["clean:portfolio", "portfolio-fonts", "htmlmin:portfolio", "portfolio-css", "copy:portfolioImages", "responsive_images:portfolio", "imagemin:portfolio"]);
+
+    grunt.registerTask('ngrok', 'Run pagespeed with ngrok', function() {
+        var done = this.async();
+        var port = 8080;
+
+        ngrok.connect(port, function(err, url) {
+            if (err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+
+            grunt.config.set('pagespeed.options.url', url);
+            grunt.task.run('pagespeed');
+            done();
+        });
+    });
 };
